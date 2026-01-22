4657dcbb1351a28e5cc9ae04e39bcea648cb5b5d
Bailey <baileyjay9@hotmail.co.uk>
2026-01-22 03:12:33 +0000

    get all endpoint and handler

--- index.ts
+++ index.ts
@@ -1,11 +1,26 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
+import VehicleRepository from './repositories/vehicle-repository';
 
 const app: Express = express();
 const port = 3000;
+const vehicleRepository = new VehicleRepository;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
-});
+// split into handlers and endpoints for unit testing
+
+export const getAll = async (req: Request, res: Response) => {
+  try {
+    const vehicles = await vehicleRepository.getAll();
+
+    res.status(200).json({
+      data: vehicles,
+      count: vehicles.length,
+    });
+  } catch (error) {
+    res.status(404).json({ message: 'Failed to fetch vehicles'})
+  }
+};
+
+app.get('/vehicles', getAll);
 
 app.listen(port, () => {
   console.log(`Running at http://localhost:${port}`);
