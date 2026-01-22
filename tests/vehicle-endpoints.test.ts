2db6be34736add3cb410ad0c23fa32ba636989e4
Bailey <baileyjay9@hotmail.co.uk>
2026-01-22 01:50:55 +0000

    define test cases

---
+++ tests/vehicle-endpoints.test.ts
@@ -0,0 +1,44 @@
+import { describe, test, expect, beforeEach } from 'vitest'
+
+describe("vehicle repository endpoint requests", () => {
+
+    test("valid GET /vehicles request returns all vehicles", async () => {
+
+    });
+
+    test("valid GET /vehicles/make request returns all vehicles by make", async () => {
+
+    });
+
+    test("valid GET /vehicles/model request returns all vehicles by model", async () => {
+
+    });
+
+    test("valid GET /vehicles/minPrice request returns all vehicles by minimum price", async () => {
+
+    });
+
+    test("valid GET /vehicles/maxPrice request returns all vehicles by maximum price", async () => {
+
+    });
+
+    test("valid GET /vehicles/minMaxPrice request returns all vehicles by minimum and maximum price", async () => {
+
+    });
+
+    test("valid GET /vehicles/mileage request returns all vehicles by mileage", async () => {
+
+    });
+
+    test("valid POST /vehicles request adds a new vehicle to the dataset", async () => {
+
+    });
+
+    test("invalid GET request returns an error message and empty array", async () => {
+
+    });
+
+    test("invalid POST request returns an error message", async () => {
+
+    });
+});
